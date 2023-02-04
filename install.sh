9ba4ce90cc386878d8c6a74a134402095c0056fb
Aptify <121524673+OfficialCJDesigns@users.noreply.github.com>
2023-02-04 15:47:30 -0600

    Update install.sh
    Signed-off-by: Aptify <121524673+OfficialCJDesigns@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -32,8 +32,10 @@ mv fixmycode ~/.local/bin/
 chmod +x ~/.local/bin/fixmycode
 mkdir /usr/share/fixmycode
 cd /usr/share/fixmycode
-wget https://example.com/.config
+wget https://raw.githubusercontent.com/OfficialCJDesigns/checkmycode/main/.config
 
 # Echo instructions for the user
+echo "Installation Complete!"
+echo "Please Follow the Instructions Below BEFORE you attempt to run the application"
 echo "Please put your OpenAI API key inside of the .config file using nano."
 echo "You can get your OpenAI API key from https://beta.openai.com/signup."
