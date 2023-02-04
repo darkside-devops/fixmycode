1c1360b7cc9826f44078b097cdf6da8899d63dc3
Aptify <121524673+OfficialCJDesigns@users.noreply.github.com>
2023-02-04 15:45:15 -0600

    Create install.sh
    Signed-off-by: Aptify <121524673+OfficialCJDesigns@users.noreply.github.com>

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Determine the package installer based on the distro of linux
+if [ -x "$(command -v apt-get)" ]; then
+  package_installer="apt-get -y install"
+elif [ -x "$(command -v yum)" ]; then
+  package_installer="yum -y install"
+elif [ -x "$(command -v dnf)" ]; then
+  package_installer="dnf -y install"
+else
+  echo "Could not determine package installer. Please install jq, curl, and git manually."
+  exit 1
+fi
+
+# Check if jq, curl, and git are installed, and install them if they are not
+if ! [ -x "$(command -v jq)" ]; then
+  $package_installer jq
+fi
+if ! [ -x "$(command -v curl)" ]; then
+  $package_installer curl
+fi
+if ! [ -x "$(command -v git)" ]; then
+  $package_installer git
+fi
+
+# Check if the fixmycode script is present in the local directory
+if [ ! -f "fixmycode" ]; then
+  # Clone the specified repository if the fixmycode script is not found
+  wget https://github.com/.config
+fi
+mv fixmycode ~/.local/bin/
+chmod +x ~/.local/bin/fixmycode
+mkdir /usr/share/fixmycode
+cd /usr/share/fixmycode
+wget https://example.com/.config
+
+# Echo instructions for the user
+echo "Please put your OpenAI API key inside of the .config file using nano."
+echo "You can get your OpenAI API key from https://beta.openai.com/signup."
